a82474651244d3be49906d286371d76a7c761489
CRIMSON <olueatoworosanwo@email.com>
2020-10-10 03:04:10 +0100

    Added email system and backend

---
+++ TodoList Pro/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+const server = express();
+dotenv.config();
+server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.post("/contact_req", async (req, res) => {
+    let details = `
+        <h2>You recieved a message from ${req.body.fullname}</h2>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+        <p>Sender's Email: <span>${req.body.email}</span></p>
+        `;
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        secure: false,
+        auth: {
+            user: process.env.email,
+            pass: process.env.password,
+        },
+        tls: {
+            rejectUnauthorized: false,
+            secureProtocol: "TLSv1_method",
+        },
+    });
+    // send mail with defined transport object
+    let maildata = {
+        from: req.body.email,
+        to: process.env.email,
+        replyTo: req.body.email,
+        subject: "You have Received a Message from Your Todo App",
+        html: details,
+    };
+    transporter.sendMail(maildata, (err) => {
+        if (err) {
+            res.json({ success: false });
+            return;
+        } else {
+            res.json({ success: true });
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log("Server Running on 3001");
+});
