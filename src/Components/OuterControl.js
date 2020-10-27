6cbc171be76750463e9d563cfaec4604067dfbce
CRIMSON <olueatoworosanwo@email.com>
2020-10-27 04:25:39 +0100

    edit

--- src/Components/OuterControl.js
+++ src/Components/OuterControl.js
@@ -213,7 +213,11 @@ function OuterControl({
                     ></div>
                 )}
             </Transition>
-            <Transition component={false} enter={{ translateX: 0 }} leave={{ translateX: -300 }}>
+            <Transition
+                component={false}
+                enter={{ translateX: 0, opacity: 1 }}
+                leave={{ translateX: -500, opacity: 0 }}
+            >
                 {side && (
                     <div key="sideBar" className={`sideBar`}>
                         <h2>
@@ -718,7 +722,7 @@ function OuterControl({
                                 </p>
                                 <p>
                                     Now, one of the most important reasons for keeping a Todo list
-                                    is the organization Organizing your tasks with a list can make
+                                    is the organization, Organizing your tasks with a list can make
                                     everything much more managable and make you feel grounded.
                                     Seeing a clear outline of your completed and uncompleted tasks
                                     will help you feel organized and mentally focused
@@ -727,15 +731,15 @@ function OuterControl({
                             <div className="info-block">
                                 <h4>Note!</h4>
                                 <p>
-                                    Video Illustrations are made with Desktop inteerface so you can
+                                    Video Illustrations are made with Desktop interface so you can
                                     see what exactly is beign clicked
                                 </p>
-                                <p>if Video Illustration dosent play, Kindly refresh the page </p>
+                                <p>if Video Illustration dosen't play, Kindly refresh the page </p>
                             </div>
                             <div className="info-block">
                                 <h4>Creating a List</h4>
                                 <p>
-                                    To create a List, Click the three line icon at Top left corner
+                                    To Create a List, Click the three line icon at Top left corner
                                     and click "Add New List", then Write the name of the list and
                                     Click Save
                                 </p>
@@ -746,7 +750,7 @@ function OuterControl({
                             <div className="info-block">
                                 <h4>Creating a Task</h4>
                                 <p>
-                                    To create a task, Simply Write it in the Input Field and press
+                                    To Create a task, wimply write it in the Input Field and press
                                     the Plus Button to add it to the List
                                 </p>
                                 <video autoPlay={true} loop={true}>
@@ -810,18 +814,18 @@ function OuterControl({
                                 <p>
                                     This is very easy to accomplish, you just need to minimize the
                                     App and the click the icon that looks like a Lock, You will have
-                                    to lock Google Chrome or any Browser used to Install this app as
-                                    the App is a extension of the Browser, if your Browser shuts
-                                    down, the app shuts down with it
+                                    to lock Google Chrome or any Browser used to Install this App is
+                                    a extension of the Browser, if your Browser shuts down, the app
+                                    shuts down with it
                                 </p>
                                 <img src={lock} alt="lock"></img>
                                 <h4>Removig the App from Optiization List</h4>
                                 <p>
                                     There are a lot of Inbuilt system Apps that constantly optimize
                                     our device for us, This sounds Good but it removes the Apps that
-                                    we to function even when were not using it, a way around this is
-                                    to remove the App from any optimization List as much as possible
-                                    for it to continue running and Reminding you in future
+                                    we need to function even when were not using it, a way around
+                                    this is to remove the App from any optimization List as much as
+                                    possible for it to continue running and Reminding you in future
                                 </p>
                                 <img src={opt} alt="optimize"></img>
                                 <p>
