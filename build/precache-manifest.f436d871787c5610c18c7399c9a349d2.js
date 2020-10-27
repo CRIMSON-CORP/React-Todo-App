ddce09743e0c217af9fe696dbe89c0b4201e5976
CRIMSON <olueatoworosanwo@email.com>
2020-10-27 02:58:18 +0100

    folder changes

---
+++ build/precache-manifest.f436d871787c5610c18c7399c9a349d2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390fbcaf6eeaffabf6ffa7563c2d2f40",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "234de77e24ba1e5dc016",
+    "url": "/React-Todo-App/static/css/2.8044c660.chunk.css"
+  },
+  {
+    "revision": "0a9c76c8bdabe882c326",
+    "url": "/React-Todo-App/static/css/main.5d4b8072.chunk.css"
+  },
+  {
+    "revision": "234de77e24ba1e5dc016",
+    "url": "/React-Todo-App/static/js/2.a64cee59.chunk.js"
+  },
+  {
+    "revision": "8510302fe673a170fabcaa705e94b96c",
+    "url": "/React-Todo-App/static/js/2.a64cee59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9c76c8bdabe882c326",
+    "url": "/React-Todo-App/static/js/main.e99640bd.chunk.js"
+  },
+  {
+    "revision": "2c22359d02144334408d",
+    "url": "/React-Todo-App/static/js/runtime-main.eb8ed301.js"
+  },
+  {
+    "revision": "bdcc0fe6a9155b3d3a448417cf327548",
+    "url": "/React-Todo-App/static/media/Create Task.bdcc0fe6.mp4"
+  },
+  {
+    "revision": "30c75d32c82d52f042d8bd0919dd51a3",
+    "url": "/React-Todo-App/static/media/Editing_Reminder.30c75d32.mp4"
+  },
+  {
+    "revision": "0c664a84b26558711d63277a07087c22",
+    "url": "/React-Todo-App/static/media/Newlist.0c664a84.mp4"
+  },
+  {
+    "revision": "f9b62236c8d90f8e1081ba976e196f62",
+    "url": "/React-Todo-App/static/media/RenameList.f9b62236.mp4"
+  },
+  {
+    "revision": "11b02c8abb579fbd07885a0f9a98ce3d",
+    "url": "/React-Todo-App/static/media/lock.11b02c8a.png"
+  },
+  {
+    "revision": "5714c6638f50792da0d7345a1b9596a5",
+    "url": "/React-Todo-App/static/media/near.5714c663.PNG"
+  },
+  {
+    "revision": "dd029f19cd9a73bd8ce777d64fafbd4c",
+    "url": "/React-Todo-App/static/media/new.dd029f19.PNG"
+  },
+  {
+    "revision": "8d0d4dc35b408ede0be0a25e5c851e46",
+    "url": "/React-Todo-App/static/media/opt.8d0d4dc3.png"
+  },
+  {
+    "revision": "eed007e1fae3ef7682b6cc06b4f4c7dc",
+    "url": "/React-Todo-App/static/media/passed.eed007e1.PNG"
+  }
+]);
