9c2ee5d0fc2e9e20198b4dbf1a4c9be46c7c2f6c
CRIMSON <olueatoworosanwo@email.com>
2020-10-30 22:18:44 +0100

    Updates

---
+++ precache-manifest.6dca0309b83d299bb5e8d362b7d9e9c8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a75328eb1a1d7cb33b63ad2c692be2f",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "0ea1b64163c4b84dd518",
+    "url": "/React-Todo-App/static/css/2.8044c660.chunk.css"
+  },
+  {
+    "revision": "bec238656626c2d5a298",
+    "url": "/React-Todo-App/static/css/main.dc1db1b6.chunk.css"
+  },
+  {
+    "revision": "0ea1b64163c4b84dd518",
+    "url": "/React-Todo-App/static/js/2.0e469ac2.chunk.js"
+  },
+  {
+    "revision": "8510302fe673a170fabcaa705e94b96c",
+    "url": "/React-Todo-App/static/js/2.0e469ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec238656626c2d5a298",
+    "url": "/React-Todo-App/static/js/main.2bfea082.chunk.js"
+  },
+  {
+    "revision": "2c22359d02144334408d",
+    "url": "/React-Todo-App/static/js/runtime-main.eb8ed301.js"
+  },
+  {
+    "revision": "bdcc0fe6a9155b3d3a448417cf327548",
+    "url": "/React-Todo-App/static/media/Create Task.bdcc0fe6.mp4"
+  },
+  {
+    "revision": "30c75d32c82d52f042d8bd0919dd51a3",
+    "url": "/React-Todo-App/static/media/Editing_Reminder.30c75d32.mp4"
+  },
+  {
+    "revision": "0c664a84b26558711d63277a07087c22",
+    "url": "/React-Todo-App/static/media/Newlist.0c664a84.mp4"
+  },
+  {
+    "revision": "f9b62236c8d90f8e1081ba976e196f62",
+    "url": "/React-Todo-App/static/media/RenameList.f9b62236.mp4"
+  },
+  {
+    "revision": "11b02c8abb579fbd07885a0f9a98ce3d",
+    "url": "/React-Todo-App/static/media/lock.11b02c8a.png"
+  },
+  {
+    "revision": "5714c6638f50792da0d7345a1b9596a5",
+    "url": "/React-Todo-App/static/media/near.5714c663.PNG"
+  },
+  {
+    "revision": "dd029f19cd9a73bd8ce777d64fafbd4c",
+    "url": "/React-Todo-App/static/media/new.dd029f19.PNG"
+  },
+  {
+    "revision": "8d0d4dc35b408ede0be0a25e5c851e46",
+    "url": "/React-Todo-App/static/media/opt.8d0d4dc3.png"
+  },
+  {
+    "revision": "eed007e1fae3ef7682b6cc06b4f4c7dc",
+    "url": "/React-Todo-App/static/media/passed.eed007e1.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a962ff78ca60799c03f495967cfe77a7.js"
+  "/React-Todo-App/precache-manifest.6dca0309b83d299bb5e8d362b7d9e9c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Todo-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
